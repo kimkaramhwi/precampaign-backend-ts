2b7ce155fe672fe5b9ba9cd5493b54cf74dad254
김가람휘 <kimkaramhwi@172-1-7-241.lightspeed.hstntx.sbcglobal.net>
2022-04-15 18:09:50 +0900

    fix: try

--- app/controllers/campaign.controller.ts
+++ app/controllers/campaign.controller.ts
@@ -5,76 +5,76 @@ import Campaign from "../models/campaign.model";
 import { ICampaignAttributes } from "../models/campaign.model";
 
 const create = (req: Request, res: Response) => {
-    try {
-        const { title, status, evaluation_start_date, evaluation_end_date, description, thumbnail_url }: ICampaignAttributes = req.body;
-        const campaign = {
-            title: title,
-            status: status,
-            evaluation_start_date: evaluation_start_date,
-            evaluation_end_date: evaluation_end_date,
-            description: description,
-            thumbnail_url: thumbnail_url
-        };
+    const { title, status, evaluation_start_date, evaluation_end_date, description, thumbnail_url }: ICampaignAttributes = req.body;
+    const campaign = {
+        title: title,
+        status: status,
+        evaluation_start_date: evaluation_start_date,
+        evaluation_end_date: evaluation_end_date,
+        description: description,
+        thumbnail_url: thumbnail_url
+    };
 
-        Campaign.create(campaign)
-            .then(data => {
-                res.send(data);
-            })
-    } catch (err) {
-        res.status(500).send({ message: err.message });
-    }
+    Campaign.create(campaign)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
 
 };
 
 const findAll = (req: Request, res: Response) => {
-    try {
-        const status = req.query.status;
-        const limit: number = parseInt(req.query.limit as string) || 9;
-        const offset: number = parseInt(req.query.offset as string) || 0;
-        const sort_by = req.query.sort_by as string || "id";
-        const sort_order = req.query.sort_order as string || "ASC";
-        const condition = status ? { status: { [Op.like]: `%${status}%` } } : null;
+    const status = req.query.status;
+    const limit: number = parseInt(req.query.limit as string) || 9;
+    const offset: number = parseInt(req.query.offset as string) || 0;
+    const sort_by = req.query.sort_by as string || "id";
+    const sort_order = req.query.sort_order as string || "ASC";
+    const condition = status ? { status: { [Op.like]: `%${status}%` } } : null;
 
-        if (sort_by === "count") {
-            Campaign.findAll({
-                where: condition,
-                limit: limit,
-                offset: offset,
-                attributes: {
-                    include: [[sequelize.literal('(SELECT COUNT(*) FROM campaign_applicant WHERE campaign_applicant.campaign_id = Campaign.id)'), 'applicant_count']]
-                },
-                order: [[sequelize.literal('applicant_count'), sort_order]]
+    if (sort_by === "count") {
+        Campaign.findAll({
+            where: condition,
+            limit: limit,
+            offset: offset,
+            attributes: {
+                include: [[sequelize.literal('(SELECT COUNT(*) FROM campaign_applicant WHERE campaign_applicant.campaign_id = Campaign.id)'), 'applicant_count']]
+            },
+            order: [[sequelize.literal('applicant_count'), sort_order]]
+        })
+            .then(data => {
+                res.send(data);
             })
-                .then(data => {
-                    res.send(data);
-                })
-        } else {
-            Campaign.findAll({
-                where: condition,
-                limit: limit,
-                offset: offset,
-                order: [[sort_by, sort_order]]
+            .catch(err => {
+                res.status(500).send({ message: err.message });
+            });
+    } else {
+        Campaign.findAll({
+            where: condition,
+            limit: limit,
+            offset: offset,
+            order: [[sort_by, sort_order]]
+        })
+            .then(data => {
+                res.send(data);
             })
-                .then(data => {
-                    res.send(data);
-                })
-        }
-    } catch (err) {
-        res.status(500).send({ message: err.message });
+            .catch(err => {
+                res.status(500).send({ message: err.message });
+            });
     }
 };
 
 const findOne = (req: Request, res: Response) => {
-    try {
-        const id = req.params.id;
+    const id = req.params.id;
 
-        Campaign.findByPk(id)
-            .then(data => {
-                res.send(data);
-            })
-    } catch (err) {
-        res.status(500).send({ message: err.message });
-    }
+    Campaign.findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 export default {
--- app/middlewares/authJwt.ts
+++ app/middlewares/authJwt.ts
@@ -3,28 +3,24 @@ import jwt from "jsonwebtoken";
 import config from "../config/config";
 
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
 
-    if (!token) {
-      return res.status(403).send({
-        message: "No token!"
+  if (!token) {
+    return res.status(403).send({
+      message: "No token!"
+    });
+  }
+
+  jwt.verify(token, config.auth.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!"
       });
     }
 
-    jwt.verify(token, config.auth.secret, (err, decoded) => {
-      if (err) {
-        return res.status(401).send({
-          message: "Unauthorized!"
-        });
-      }
+    req.userId = decoded["id"];
 
-      req.userId = decoded["id"];
-
-      next();
-    });
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
+    next();
+  });
 };
 
